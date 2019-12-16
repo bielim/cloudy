97a9c778960cbebe86d22670b77d8f82c56e4469
Melanie Bieli <melanie.bieli@bluewin.ch>
2019-12-15 22:21:39 -0500

    Rename modules

--- src/Cloudy.jl
+++ src/Cloudy.jl
@@ -3,5 +3,9 @@ module Cloudy
 include("KernelTensors/KernelTensors.jl")
 include("Distributions/Distributions.jl")
 include("Sources/Sources.jl")
+include("PerfectModel/Truth.jl")
+include("PerfectModel/EKI.jl")
+include("PerfectModel/GPEmulator.jl")
+include("PerfectModel/MCMC.jl")
 
 end
